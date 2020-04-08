e2156aafdf81e89710bde3e9ba14cf25ce121453
bzda-git <61729228+bzda-git@users.noreply.github.com>
2020-04-08 19:41:39 +0300

    Nie działa jak trzeba

---
+++ Models/Request/PromotionRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw6.Models
+{
+    public class PromotionRequest
+    {
+        [Required(ErrorMessage = "Musisz podać kierunek studiów")]
+        public string Studies { get; set; }
+        [Required(ErrorMessage = "Musisz podać semester")]
+        public int Semester { get; set; }
+    }
+}
