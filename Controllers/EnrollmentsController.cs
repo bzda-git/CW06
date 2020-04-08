fabf17ca2412f678a53e7777a105c9d457fcd5fe
bzda-git <61729228+bzda-git@users.noreply.github.com>
2020-04-08 20:34:27 +0300

    CW5 nie zakonczone

---
+++ Controllers/EnrollmentsController.cs
@@ -0,0 +1,44 @@
+﻿using Cw6.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Services;
+
+namespace Cw6.Controllers
+{
+    [Route("api/enrollments")]
+    [ApiController]
+    public class EnrollmentsController : ControllerBase
+    {
+
+
+        private IDbService _service;
+
+        public EnrollmentsController(IDbService service)
+        {
+            _service = service;
+
+        }
+
+        [HttpPost]
+       // public IActionResult EnrollStudent(EnrollStudentRequest request)
+    //    {
+
+        //    var response = _service.EnrollStudent(request);
+       //     return Ok(response);
+
+
+     //   }
+
+        [HttpPost("promotions")]
+        public IActionResult PromoteStudents(PromotionRequest promotionRequest)
+        {
+
+            var response = _service.PromoteStudents(promotionRequest);
+            return Ok(response);
+        }
+    }
+    
+}
---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using WebApplication2.Models;
+using WebApplication2.Services;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService; 
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            var list = new List<Student>()
+            {
+                new Student{IndexNumber=1, FirstName="Jan", LastName="Kowalski"},
+                new Student{IndexNumber=2, FirstName="Andrzej", LastName="Malewicz"},
+                new Student{IndexNumber=3, FirstName="Krzysztof", LastName="Andrzejewicz"}
+            };
+
+            
+
+            return Ok(list);
+        }
+
+
+
+    }
+}
