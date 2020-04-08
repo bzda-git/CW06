4ce7fbf0da553ed5eb587e060515e8076dde59bb
bzda-git <61729228+bzda-git@users.noreply.github.com>
2020-04-08 19:35:24 +0300

    CW_06
    Cw6 musi działać, ale potrzebuje dopisać cw5.

---
+++ Startup.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using WebApplication2.Middlewares;
+using WebApplication2.Services;
+
+namespace WebApplication2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // dodajemy zależność, żeby móc wstrzykiwać nasz interfejs w programie m.in. do metody Configure klasy Startup
+            services.AddTransient<IDbService, DbService>();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IDbService dbService)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMiddleware<LoggingMiddleware>();
+
+            app.Use(async (context, next) =>
+            {
+                if (!context.Request.Headers.ContainsKey("Index"))
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("Nie podany indeks");
+                    return;
+                }
+
+                var index = context.Request.Headers["Index"].ToString();
+                //check in db
+                if (!dbService.CheckIndex(index))
+                {
+                    // jak nie istnieje to informujemy o tym użytkownika
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("Nie ma studenta w bazie danych");
+                    return;
+                }
+
+                await next();
+            });
+
+
+
+            app.UseRouting();  
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
